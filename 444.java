fbaa705b45ab5ad631f1721c6b215231f8143527
Ruslan Nazarenko <boss.rustaman9@gmail.com>
2022-04-25 11:36:24 +0300

    first commit

--- 444.java
+++ 444.java
@@ -4,12 +4,5 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class CSS {
-    public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://eplio.com.ua/");
-        WebElement input = driver.findElement(By.cssSelector("[href='https://eplio.com.ua/iphone/']"));
-        input.click();
 
 
